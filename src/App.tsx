73684adc134d2575f33137d618915ee23e5f5790
jean-paulo <jeanprs@msn.com>
2020-08-03 11:37:41 -0300

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './assets/styles/global.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Hello World</h1>
     </div>
   );
 }
