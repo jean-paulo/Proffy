4bee14214b0754c211840bbc4091c1b60ec6adc1
jean-paulo <jeanprs@msn.com>
2020-08-04 19:42:59 -0300

    Criação e listagem de connections

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 // Modulo de roteamento do Express
 const routes = express.Router();
 const classesControllers = new ClassesController();
+const connectionsControllers = new ConnectionsController();
 
 routes.post('/classes', classesControllers.create);
 routes.get('/classes', classesControllers.index);
+
+
+routes.post('/connections', connectionsControllers.create);
+routes.get('/connections', connectionsControllers.index);
 
 
 export default routes;
