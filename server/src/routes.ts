09ff00a9d7de021540db03fe763a60ceed88cf18
jean-paulo <jeanprs@msn.com>
2020-08-04 19:20:12 -0300

    Listagem das aulas completa

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,75 +1,12 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
 
 // Modulo de roteamento do Express
 const routes = express.Router();
+const classesControllers = new ClassesController();
 
-interface ScheduleItem {
-    week_day: number;
-    from: string;
-    to: string;
-}
-
-
-routes.post('/classes', async (request, response) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body;
-
-    // Transaction
-    const trx = await db.transaction()
-
-    try {
-        const insertedUsersIds = await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio,
-        });
-
-        const user_id = insertedUsersIds[0];
-
-        const insertedClassesIds = await trx('classes').insert({
-            subject,
-            cost,
-            user_id,
-        });
-
-        const class_id = insertedClassesIds[0];
-
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: convertHourToMinutes(scheduleItem.from),
-                to: convertHourToMinutes(scheduleItem.to),
-            };
-        })
-
-        await trx('class_schedule').insert(classSchedule);
-
-        // Se todas as operações obtiverem sucesso, salva no banco
-        await trx.commit();
-
-
-        return response.status(201).send();
-    } catch (err) {
-        //Quando usamos o try/catch é importante fazer o rollback no erro
-        //Desfaz qualquer alteração que tenha acontecido no banco na mesma requisição até o erro.
-        await trx.rollback();
-
-        return response.status(400).json({
-            error: 'Unexpected error while creating new class'
-        });
-    }
-});
+routes.post('/classes', classesControllers.create);
+routes.get('/classes', classesControllers.index);
 
 
 export default routes;
